07d937578cc4221843f65d9bd8771624d0b52b2d
Gregtmj <greg_1461996@hotmail.com>
2022-12-09 11:07:06 +0300

    changing in uesrs url edit to update

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -7,7 +7,7 @@ from task_manager.users.views import UserListView, CreateUserView,\
 urlpatterns = [
     path('users/', UserListView.as_view(), name='users'),
     path('users/create/', CreateUserView.as_view(), name='register'),
-    path('users/<int:pk>/edit/', UserUpdateView.as_view(), name='edit'),
+    path('users/<int:pk>/update/', UserUpdateView.as_view(), name='edit'),
     path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='delete'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
