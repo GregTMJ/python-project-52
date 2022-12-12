4a70ab0d97d5cd990f49f77b1f2b1e3d472e08f1
Gregtmj <greg_1461996@hotmail.com>
2022-12-12 09:11:39 +0300

    changing form for labels in tasks

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,8 +1,6 @@
 from django import forms
-from django.utils.translation import gettext_lazy as _
 
 from task_manager.tasks.models import Task
-from task_manager.labels.models import Label
 
 
 class CreateTaskForm(forms.ModelForm):
@@ -10,10 +8,3 @@ class CreateTaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'executor', 'status', 'label']
-
-    label = forms.ModelMultipleChoiceField(
-        queryset=Label.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=False,
-        label=_('labels'),
-    )
